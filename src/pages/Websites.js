02c786cd1375d7cdc1363dc7dd2c0b5bcb41a1ea
Carlos Acosta <gitcodingcarlos@gmail.com>
2024-05-24 16:57:52 -0600

    Selina's Shop

--- src/pages/Websites.js
+++ src/pages/Websites.js
@@ -2,33 +2,39 @@ import React from 'react'
 import redPotion from "../assets/redPotiongif.gif";
 import greenPotion from "../assets/greenPotiongif.gif";
 import purplePotion from "../assets/purplePotiongif.gif";
+import lilacPotion from "../assets/lilacPotion.gif";
 import { useTranslation } from 'react-i18next';
 import "../styles/Websites.css";
 
-function Websites( )  {
+function Websites() {
 
   const { t } = useTranslation();
 
   return (
     <>
-        <div className="menu">
-            <h1 className="menuTitle" > {t('description.website1')} </h1>
-            <div className="websites">
-              <a href="https://mariosautoservice.netlify.app/" className="potions">
-                    <img src={redPotion} alt="Red potion to Mario Website" id="red_potion" />
-              </a>
-    
-              <a href="https://llanterialuigi.netlify.app/" className="potions">
-                    <img src={greenPotion}  alt="Green potion to Luigi Website" id="green_potion" />
-            </a>
-              <a href="https://elcompawaluigi.netlify.app/">
-                <img src={purplePotion} alt="Purple potion to Waluigi Website" id="purple_potion" />
-              </a>
+      <div className="menu">
+        <h1 className="menuTitle" > {t('description.website1')} </h1>
+        <div className="websites">
+          <a href="https://mariosautoservice.netlify.app/" className="potions">
+            <img src={redPotion} alt="Red potion to Mario Website" id="red_potion" />
+          </a>
 
-              </div>
+          <a href="https://llanterialuigi.netlify.app/" className="potions">
+            <img src={greenPotion} alt="Green potion to Luigi Website" id="green_potion" />
+          </a>
+          <a href="https://elcompawaluigi.netlify.app/">
+            <img src={purplePotion} alt="Purple potion to Waluigi Website" id="purple_potion" />
+          </a>
 
-      
+          <a href="https://selinasshop.netlify.app/">
+            <img src={lilacPotion} alt="Lilac potion to Selina's Website" id="lilac_potion" />
+          </a>
+
+
         </div>
+
+
+      </div>
 
     </>
   )
